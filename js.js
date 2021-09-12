7115376d695bbff7cb7f2cdac85f1ce6bec84a5c
Sevinch-najmiddinova <najmiddinova2021@mail.ru>
2021-09-12 15:16:17 +0500

    save last
    commit

---
+++ js.js
@@ -0,0 +1,47 @@
+const divEl = document.getElementsByClassName('color')[0];
+const divEl1 = document.getElementsByClassName('color')[1];
+const divEl2 = document.getElementsByClassName('color')[2];
+const divEl3 = document.getElementsByClassName('color')[3];
+const divEl4 = document.getElementsByClassName('color')[4];
+const divEl5 = document.getElementsByClassName('color')[5];
+
+
+divEl.addEventListener('mouseenter', function () {
+    divEl.style.backgroundColor = 'white';
+});
+divEl.addEventListener('mouseleave', function () {
+    divEl.style.backgroundColor = '';
+});
+
+divEl1.addEventListener('mouseenter', function () {
+    divEl1.style.backgroundColor = 'white';
+});
+divEl1.addEventListener('mouseleave', function () {
+    divEl1.style.backgroundColor = '';
+});
+
+divEl2.addEventListener('mouseenter', function () {
+    divEl2.style.backgroundColor = 'white';
+});
+divEl2.addEventListener('mouseleave', function () {
+    divEl2.style.backgroundColor = '';
+});
+
+divEl3.addEventListener('mouseenter', function () {
+    divEl3.style.backgroundColor = 'white';
+});
+divEl3.addEventListener('mouseleave', function () {
+    divEl3.style.backgroundColor = '';
+});
+divEl4.addEventListener('mouseenter', function () {
+    divEl4.style.backgroundColor = 'white';
+});
+divEl4.addEventListener('mouseleave', function () {
+    divEl4.style.backgroundColor = '';
+});
+divEl5.addEventListener('mouseenter', function () {
+    divEl5.style.backgroundColor = 'white';
+});
+divEl5.addEventListener('mouseleave', function () {
+    divEl5.style.backgroundColor = '';
+});
